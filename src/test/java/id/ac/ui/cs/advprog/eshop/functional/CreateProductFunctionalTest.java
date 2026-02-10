0f9492ca4c48a6a8a9115eceba77422cfe664f6b
unknown <seanmarcello836@gmail.com>
2026-02-10 18:58:44 +0700

    create functional test for create product

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -0,0 +1,66 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+class CreateProductFunctionalTest {
+
+    @LocalServerPort
+    private int serverPort;
+
+    private String baseUrl;
+
+    @BeforeEach
+    void setupTest() {
+        baseUrl = String.format("http://localhost:%d", serverPort);
+    }
+
+    @Test
+    void createProduct_isCorrect(ChromeDriver driver) throws Exception {
+        driver.get(baseUrl + "/product/create");
+
+        String inputName = "Sampo Cap Bambang";
+        int inputQuantity = 100;
+
+        WebElement nameInput = driver.findElement(By.id("nameInput"));
+        nameInput.clear();
+        nameInput.sendKeys(inputName);
+
+        WebElement quantityInput = driver.findElement(By.id("quantityInput"));
+        quantityInput.clear();
+        quantityInput.sendKeys(String.valueOf(inputQuantity));
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitButton.click();
+
+        String currentUrl = driver.getCurrentUrl();
+        assertTrue(currentUrl.contains("/product/list"));
+
+        List<WebElement> tableCells = driver.findElements(By.tagName("td"));
+
+        boolean isProductFound = false;
+        for (WebElement cell : tableCells) {
+            if (cell.getText().equals(inputName)) {
+                isProductFound = true;
+                break;
+            }
+        }
+
+        assertTrue(isProductFound, "Product name should be visible in the product list table");
+    }
+}
