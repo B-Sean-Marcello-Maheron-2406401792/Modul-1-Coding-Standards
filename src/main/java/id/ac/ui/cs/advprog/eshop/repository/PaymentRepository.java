2ddccac186fa6433976fce9840e3fb988cc9a09f
unknown <seanmarcello836@gmail.com>
2026-03-10 17:37:32 +0700

    [REFACTOR] Apply Java Streams and removeIf to PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -11,23 +11,16 @@ public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
     public Payment save(Payment payment) {
-        for (int i = 0; i < paymentData.size(); i++) {
-            if (paymentData.get(i).getId().equals(payment.getId())) {
-                paymentData.set(i, payment);
-                return payment;
-            }
-        }
+        paymentData.removeIf(p -> p.getId().equals(payment.getId()));
         paymentData.add(payment);
         return payment;
     }
 
     public Payment findById(String id) {
-        for (Payment payment : paymentData) {
-            if (payment.getId().equals(id)) {
-                return payment;
-            }
-        }
-        return null;
+        return paymentData.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 
     public List<Payment> getAllPayments() {
